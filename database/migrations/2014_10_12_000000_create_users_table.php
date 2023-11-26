006844d6fbf8558e18bc9f18657d766742c1c4f8
Michel González <michiwoo.web@gmail.com>
2023-11-26 14:12:51 -0600

    :sparkles: Agregando filament al proyecto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('departamento');
-            $table->integer('categoria');
+            $table->string('name');
+            $table->string('departamento')->nullable();
+            $table->integer('categoria')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->timestamp('fecha_baja');
+            $table->timestamp('fecha_baja')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
