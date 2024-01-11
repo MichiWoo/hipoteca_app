8942a1bb7cc4e33e30b685e47d441b6df477e43d
Michel González <michiwoo.web@gmail.com>
2024-01-10 22:02:48 -0600

    :fire: Plis

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@woo.com') && $this->hasVerifiedEmail();
     }
 }
