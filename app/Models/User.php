110d6a6ffc455fc5e9208d26e33249c180705ab9
Michel González <michiwoo.web@gmail.com>
2024-01-13 19:31:23 -0600

    :fire: Cambiando dominio de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@woo.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@hipoteca10.es') && $this->hasVerifiedEmail();
     }
 }
